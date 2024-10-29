b161001a7232dcf6eaa3dde7560160f648cd3dd4
SmidMarko <marko.smid10@gmail.com>
2024-10-29 11:29:09 +0100

    Code cleanup

--- GenIATraP/Assets/Examples/Robostrike/Scripts/AmmoComponent.cs
+++
@@ -1,6 +0,0 @@
-using UnityEngine;
-
-public class AmmoComponent : MonoBehaviour
-{
-    public int Ammo { get; set; }
-}
--- GenIATraP/Assets/Scripts/Coordinator.cs
+++ GenIATraP/Assets/Scripts/Coordinator.cs
@@ -19,8 +19,6 @@ public class Coordinator : MonoBehaviour
     [Header("HTTP Server Configuration")]
     [SerializeField] public string CoordinatorURI = "http://localhost:4000/";
 
-
-
     private HttpListener Listener;
     private Thread ListenerThread;
 
@@ -119,97 +117,12 @@ public class Coordinator : MonoBehaviour
     /// </summary>
     IEnumerator CordinateEvaluation(HttpListenerContext context)
     {
-        // Read body from the request
-        if (!context.Request.HasEntityBody)
-        {
-            throw new Exception("No client data was sent with the request.");
-        }
-        System.IO.Stream body = context.Request.InputStream;
-        System.Text.Encoding encoding = context.Request.ContentEncoding;
-        System.IO.StreamReader reader = new System.IO.StreamReader(body, encoding);
-
-        // Convert the data to a string and display it on the console.
-        string s = reader.ReadToEnd();
-        CoordinatorEvalRequestData coordinatorEvalRequestData = JsonConvert.DeserializeObject<CoordinatorEvalRequestData>(s);
-        Debug.Log("Coordinator Cordinate Evaluation " + coordinatorEvalRequestData.evalRangeStart + "-" + coordinatorEvalRequestData.evalRangeEnd + ", EvalInstances: " + coordinatorEvalRequestData.evalEnvInstancesToString());
-
-        int numOfIndividuals = coordinatorEvalRequestData.evalRangeEnd;
-        int numOfInstances = coordinatorEvalRequestData.evalEnvInstances.Length;
-
-        int numOfIndividualsPerInstance = numOfIndividuals / numOfInstances;
-        int remainder = numOfIndividuals % numOfInstances;
-
-        using (HttpClient client = new HttpClient())
-        {
-            client.Timeout = TimeSpan.FromMinutes(120); // Set timeout to 120 minutes
-
-            Task<HttpResponseMessage>[] tasks = new Task<HttpResponseMessage>[numOfInstances];
-            for (int i = 0; i < numOfInstances; i++)
-            {
-                tasks[i] = client.PostAsync(coordinatorEvalRequestData.evalEnvInstances[i], new StringContent(JsonConvert.SerializeObject(new EvalRequestData() { evalRangeStart = i * numOfIndividualsPerInstance, evalRangeEnd = i * numOfIndividualsPerInstance + numOfIndividualsPerInstance + (i == numOfInstances - 1 ? remainder : 0) }), Encoding.UTF8, "application/json"));
-            }
-
-            while (!Task.WhenAll(tasks).IsCompleted)
-            {
-                yield return null; // Wait for tasks to complete
-            }
-
-            FitnessIndividual[] FinalPopFitnesses = new FitnessIndividual[numOfIndividuals];
-            int[][] FinalBtsNodeCallFrequencies = new int[numOfIndividuals][];
-            foreach (Task<HttpResponseMessage> task in tasks)
-            {
-                HttpResponseMessage response = task.Result;
-                if (response.IsSuccessStatusCode)
-                {
-                    var taskContent = response.Content.ReadAsStringAsync();
-                    while(!taskContent.IsCompleted)
-                    {
-                        yield return null; // Wait for task to complete
-                    }
-
-                    string result = taskContent.Result;
-
-                    HttpServerResponse responseObject = JsonConvert.DeserializeObject<HttpServerResponse>(result);
-                    if (responseObject == null)
-                    {
-                        Debug.LogError("Response object is null");
-                        UnityUtils.WriteErrorToFile("Response object is null", "CoordinatorError.txt");
-                    }
-
-                    for (int i = responseObject.EvalRequestData.evalRangeStart; i < responseObject.EvalRequestData.evalRangeEnd; i++)
-                    {
-                        FinalPopFitnesses[i] = responseObject.PopFitness[i - responseObject.EvalRequestData.evalRangeStart];
-                        FinalBtsNodeCallFrequencies[i] = responseObject.BtsNodeCallFrequencies[i - responseObject.EvalRequestData.evalRangeStart];
-                    }
-                }
-                else
-                {
-                    Debug.LogError($"Request failed with status code: {response.StatusCode}");
-                    UnityUtils.WriteErrorToFile("Response object is null", "CoordinatorError.txt");
-                }
-            }
-
-            // Return the final population fitnesses and BTS node call frequencies
-            HttpServerResponse finalResponse = new HttpServerResponse()
-            {
-                PopFitness = FinalPopFitnesses,
-                BtsNodeCallFrequencies = FinalBtsNodeCallFrequencies,
-                EvalRequestData = new EvalRequestData() { evalRangeStart = coordinatorEvalRequestData.evalRangeStart, evalRangeEnd = coordinatorEvalRequestData.evalRangeEnd }
-            };
-
-            string responseJson = JsonConvert.SerializeObject(finalResponse);
-
-            byte[] buffer = Encoding.UTF8.GetBytes(responseJson);
-            context.Response.ContentLength64 = buffer.Length;
-            context.Response.OutputStream.Write(buffer, 0, buffer.Length);
-            context.Response.OutputStream.Close();
-        }
-
+        // TODO Implement
         yield return null;
     }
 }
 
-public class CoordinatorEvalRequestData
+public class EvalRequestData
 {
     public string[] evalEnvInstances { get; set; }
     public int evalRangeStart { get; set; }
