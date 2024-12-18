d092b1793e85fdd5bfeb93943ad43f53667f012b
SmidMarko <marko.smid10@gmail.com>
2024-12-18 15:25:33 +0100

    Evo monitor initial commit.

---
+++ evo_monitor/src/components/BarChart.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+    import { ChartConfig } from '@/models/chartConfig';
+    import {
+        Chart as ChartJS,
+        Title,
+        Tooltip,
+        Legend,
+        BarElement,
+        CategoryScale,
+        LinearScale
+    } from 'chart.js';
+    import { Bar } from 'vue-chartjs';
+
+    ChartJS.register(CategoryScale, LinearScale, BarElement, Title, Tooltip, Legend);
+
+    const props = defineProps({
+        chartConfig: ChartConfig,
+    });
+
+</script>
+
+<template>
+    <Bar :id="props.chartConfig.id" :options="props.chartConfig.options" :data="props.chartConfig.data" />
+</template>
---
+++ evo_monitor/src/components/LineChart.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+    import { ChartConfig } from '@/models/chartConfig';
+    import { Chart as ChartJS, CategoryScale, LinearScale, PointElement, LineElement, Title, Tooltip, Legend } from 'chart.js';
+    import { Line } from 'vue-chartjs';
+
+    ChartJS.register(CategoryScale, LinearScale, PointElement, LineElement, Title, Tooltip, Legend);
+
+    const props = defineProps({
+        chartConfig: ChartConfig,
+    });
+
+</script>
+
+<template>
+    <Line :id="props.chartConfig.id" :options="props.chartConfig.options" :data="props.chartConfig.data" />
+</template>
