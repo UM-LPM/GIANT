f203bfc22dbd0bf4e2b9ea698fa38a0b51ca2cca
SmidMarko <marko.smid10@gmail.com>
2025-01-19 18:32:26 +0100

    Add new problem domain (Soccer). Init commit.

---
+++ GenIATraP/Assets/ProblemDomains/Soccer/Scripts/GoalComponent.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Problems.Soccer
+{
+    public class GoalComponent : MonoBehaviour
+    {
+        [SerializeField] public SoccerUtils.SoccerTeam Team;
+    }
+}
---
+++ GenIATraP/Assets/ProblemDomains/Soccer/Scripts/SoccerAgentComponent.cs
@@ -0,0 +1,9 @@
+using Base;
+
+namespace Problems.Soccer
+{
+    public class SoccerAgentComponent : AgentComponent
+    {
+
+    }
+}
